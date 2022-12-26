116a735a1628bf0b27ebe49ba610fb9bfde94067
Pavel <lp.qroovemaster@gmail.com>
2022-12-26 11:59:56 +0300

    Task 02 - ready

---
+++ src/Task02/Hello.java
@@ -0,0 +1,11 @@
+package Task02;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner inputter = new Scanner(System.in);
+        System.out.println("What is your name, dear user? Please type:");
+        System.out.println("Hello " + inputter.nextLine() + "!");
+    }
+}
