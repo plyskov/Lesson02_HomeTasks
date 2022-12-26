46e75965c3ef3b164494d6ce87d7dd9b3f975345
Pavel <lp.qroovemaster@gmail.com>
2022-12-26 17:21:03 +0300

    Task 08 - fixed

--- src/Task_08/CorrectDataTypes.java
+++ src/Task_08/CorrectDataTypes.java
@@ -5,12 +5,12 @@ public class CorrectDataTypes {
     public static void main(String[] args) {
 
         int numberOne = 10;
-        double numberTwo = 20.0;
+        double numberTwo = 20.345;
 
         System.out.println("Number 1 = " + numberOne);
         System.out.println("Number 2 = " + numberTwo);
 
-        float sum = numberOne + numberTwo;
+        double sum = numberOne + numberTwo;
 
         System.out.println("Sum = " + sum);
     }
