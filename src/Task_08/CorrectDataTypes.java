0abab2a4258f6e08b9372521ecf55e4fb06fd5c9
Pavel <lp.qroovemaster@gmail.com>
2022-12-26 17:13:32 +0300

    Task 08 - beginning, wrong code added

--- src/Task_08/CorrectDataTypes.java
+++ src/Task_08/CorrectDataTypes.java
@@ -1,4 +1,18 @@
 package Task_08;
 
 public class CorrectDataTypes {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        double numberTwo = 20.0;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        float sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
 }
