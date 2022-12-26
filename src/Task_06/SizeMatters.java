f79bf2833b50dd9c1af059c05163af92c039046b
Pavel <lp.qroovemaster@gmail.com>
2022-12-26 17:08:11 +0300

    Task 06 - fixed

--- src/Task_06/SizeMatters.java
+++ src/Task_06/SizeMatters.java
@@ -2,7 +2,7 @@ package Task_06;
 
 class SizeMatters {
     public static void main(String[] args) {
-        system.out.println("Sorry, I have an errors.");
-        system.out.println("Please fix me!")
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
     }
 }
