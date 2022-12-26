1ffa8cbf15654f512038eee0ed9e8e02720d663e
Pavel <lp.qroovemaster@gmail.com>
2022-12-26 17:06:44 +0300

    Task 06 - beginning, wrong code

---
+++ src/Task_06/SizeMatters.java
@@ -0,0 +1,8 @@
+package Task_06;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        system.out.println("Sorry, I have an errors.");
+        system.out.println("Please fix me!")
+    }
+}
