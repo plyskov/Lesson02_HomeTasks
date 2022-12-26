2518db36afd7677a8b9516e35c0d942bbd60a2e4
Pavel <lp.qroovemaster@gmail.com>
2022-12-26 17:11:29 +0300

    Task 07 - fixed

--- src/Task_07/SumTwoNumbers.java
+++ src/Task_07/SumTwoNumbers.java
@@ -4,14 +4,11 @@ public class SumTwoNumbers {
 
     public static void main(String[] args) {
 
-        int numberOne;
-        int numberTwo;
+        int numberOne = 10;
+        int numberTwo = 20;
 
         System.out.println("Number 1 = " + numberOne);
         System.out.println("Number 2 = " + numberTwo);
-
-        int numberOne = 10;
-        int numberTwo = 20;
 
         int sum = numberOne + numberTwo;
 
